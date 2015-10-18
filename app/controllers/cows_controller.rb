5ac46b81313429722fab09f96cba68a6998e236b
Steven Leiva <leiva.steven@gmail.com>
2015-10-18 19:43:14 -0400

    Define a create action in the Cows controller.

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -6,4 +6,14 @@ class CowsController < ApplicationController
   def new
     @cow = Cow.new
   end
+
+  def create
+    @cow = Cow.new(params[:cow])
+
+    if @cow.save
+      redirect_to cows_path
+    else
+      render :new
+    end
+  end
 end
